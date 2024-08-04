b40a11fd2dba5bad01dacf4dc98080d8d1648585
Camilo.ATP <camilo.antonio.t@gmail.com>
2024-08-04 18:13:49 -0400

    Se agregaron los metodos getEventById, deleteEvent y updateEvent

--- src/main/java/com/example/demo/service/EventService.java
+++ src/main/java/com/example/demo/service/EventService.java
@@ -1,11 +1,13 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Event;
+import com.example.demo.model.EventLocation;
 import com.example.demo.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EventService {
@@ -18,5 +20,22 @@ public class EventService {
 
     public Event createEvent(Event event) {
         return eventRepository.save(event);
+    }
+
+    public Optional<Event> getEventById(String id) {
+        return eventRepository.findById(id);
+    }
+
+    // Eliminar un evento
+    public void deleteEvent(String id) {
+        eventRepository.deleteById(id);
+    }
+
+    // Actualizar un evento
+    public Event updateEvent(String id, Event event) {
+        Event eventToUpdate = eventRepository.findById(id).get();
+        eventToUpdate.setName(event.getName());
+        eventToUpdate.setDate(event.getDate());
+        return eventRepository.save(eventToUpdate);
     }
 }
