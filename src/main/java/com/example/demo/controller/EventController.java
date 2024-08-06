b834647fbe93efd638ca273c65ddd307656e394d
Camilo.ATP <camilo.antonio.t@gmail.com>
2024-08-06 05:59:40 -0400

    EventController: se agregaron los metodos getEventsByCategoryId, getEventsRandom, getEventsByLocalId y getEventsByName

--- src/main/java/com/example/demo/controller/EventController.java
+++ src/main/java/com/example/demo/controller/EventController.java
@@ -38,4 +38,28 @@ public class EventController {
     public Event updateEvent(@PathVariable String id, @RequestBody Event event) {
         return eventService.updateEvent(id, event);
     }
+
+    // obtener eventos por categoria id
+    @GetMapping("/category/{categoryId}")
+    public List<Event> getEventsByCategoryId(@PathVariable String categoryId) {
+        return eventService.getEventsByCategoryId(categoryId);
+    }
+
+    // obtener eventos random
+    @GetMapping("/random")
+    public List<Event> getEventsRandom() {
+        return eventService.getEventsRandom();
+    }
+
+    // obtener eventos por local
+    @GetMapping("/local/{localId}")
+    public List<Event> getEventsByLocalId(@PathVariable String localId) {
+        return eventService.getEventsByLocalId(localId);
+    }
+
+    // obtener eventos por nombre
+    @GetMapping("/name/like/{name}")
+    public List<Event> getEventsByName(@PathVariable String name) {
+        return eventService.getEventsByNameLike(name);
+    }
 }
