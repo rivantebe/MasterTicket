c224c92c9ec21f3c0880aeb4879b1c5dd65aac2d
Camilo.ATP <camilo.antonio.t@gmail.com>
2024-08-06 05:56:39 -0400

    Fueron agregados los metodos getEventsByCategoryId, getEventsRandom, getEventsByLocalId y getEventsByNameLike

--- src/main/java/com/example/demo/service/EventService.java
+++ src/main/java/com/example/demo/service/EventService.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Event;
-import com.example.demo.model.EventLocation;
 import com.example.demo.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,5 +36,35 @@ public class EventService {
         eventToUpdate.setName(event.getName());
         eventToUpdate.setDate(event.getDate());
         return eventRepository.save(eventToUpdate);
+    }
+
+    // obtener eventos por nombre de categoria
+    public List<Event> getEventsByCategoryId(String categoryId) {
+        return eventRepository.findByEventCategoryId(categoryId);
+    }
+
+    // obtener eventos modo random en cada nueva petición
+    public List<Event> getEventsRandom() {
+        // reordenar la lista de eventos de forma aleatoria
+        List<Event> events = eventRepository.findAll();
+
+        for (int i = 0; i < events.size(); i++) {
+            int randomIndex = (int) (Math.random() * events.size());
+            Event temp = events.get(i);
+            events.set(i, events.get(randomIndex));
+            events.set(randomIndex, temp);
+        }
+
+        return events;
+    }
+
+    // obtener eventos por local
+    public List<Event> getEventsByLocalId(String localId) {
+        return eventRepository.findByEventLocationId(localId);
+    }
+
+    // Bucar eventos por nombre
+    public List<Event> getEventsByNameLike(String name) {
+        return eventRepository.findByNameLike(name);
     }
 }
