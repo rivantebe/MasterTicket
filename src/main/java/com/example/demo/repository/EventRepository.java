89d69fa489ad7dcf78ec922f123aadf3c06d6721
Camilo.ATP <camilo.antonio.t@gmail.com>
2024-08-06 05:58:03 -0400

    Fueron agregados findByEventCategoryId, findByEventLocationId y findByNameLike

--- src/main/java/com/example/demo/repository/EventRepository.java
+++ src/main/java/com/example/demo/repository/EventRepository.java
@@ -1,9 +1,17 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Event;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends MongoRepository<Event, String> {
+    List<Event> findByEventCategoryId(String categoryId);
+    List<Event> findByEventLocationId(String localId);
+    @Query("{ 'name': { $regex: ?0, $options: 'i' } }")
+    List<Event> findByNameLike(String name);
 }
