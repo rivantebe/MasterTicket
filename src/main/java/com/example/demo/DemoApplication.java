1acb750240d01f945144961b9feecf01384aff43
Camilo.ATP <camilo.antonio.t@gmail.com>
2024-08-06 05:50:26 -0400

    Se agrego el tag @EnableMongoAuditing para habilitar el tema de las fechas

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableMongoAuditing
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
